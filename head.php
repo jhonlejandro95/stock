1f36272d684d1cc4452e69638e38c391dac97d7f
alejandro <61956707+jhonlejandro95@users.noreply.github.com>
2020-06-10 05:53:52 -0500

    Create head.php

---
+++ head.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>almacen</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="index.php">PHP - FULLSTACK</a>
+        <div class=" navbar">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <button id="b2"  class="btn btn-outline-success my-2 my-sm-0" type="submit">Venta</button>
+                    <button id="b1" class="btn btn-outline-success my-2 my-sm-0" type="submit">Stock</button>
+                </li>
+            </ul>
+        </div>
+    </nav>
